4cb8b7d27ca747c2050feb4062949d2df83347d4
SHEAMANG <ramsford.sheamang2@mail.dcu.ie>
2024-12-06 16:30:24 +0100

    v0.75

--- src/components/PhotoGrid/PhotoGrid.js
+++ src/components/PhotoGrid/PhotoGrid.js
@@ -1,72 +1,62 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useLayoutEffect } from 'react';
 import { ReactLenis, useLenis } from 'lenis/react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import imagesLoaded from 'imagesloaded';
 import '../../styles/PhotoGrid.css';
-import vignette1 from "../../assets/images/vignettes/1.png";
-import vignette2 from "../../assets/images/vignettes/2.png";
-import vignette3 from "../../assets/images/vignettes/3.png";
-import vignette4 from "../../assets/images/vignettes/4.png";
-import vignette5 from "../../assets/images/vignettes/5.png";
-import vignette6 from "../../assets/images/vignettes/6.png";
-import vignette7 from "../../assets/images/vignettes/7.png";
-import vignette8 from "../../assets/images/vignettes/8.png";
-import vignette9 from "../../assets/images/vignettes/9.png";
-import vignette10 from "../../assets/images/vignettes/10.png";
-import vignette11 from "../../assets/images/vignettes/11.png";
-import vignette12 from "../../assets/images/vignettes/nass_shop-min.gif";
-import vignette13 from "../../assets/images/vignettes/nass_shop_1-min.gif";
-import vignette14 from "../../assets/images/vignettes/12.png";
-import vignette15 from "../../assets/images/vignettes/13.png";
-import vignette16 from "../../assets/images/vignettes/14.png";
-import vignette17 from "../../assets/images/vignettes/15.png";
-import vignette18 from "../../assets/images/vignettes/16.png";
-import vignette19 from "../../assets/images/vignettes/17.png";
 
+import vignette1 from "../../assets/images/vignettes/1.webp";
+import vignette2 from "../../assets/images/vignettes/2.webp";
+import vignette3 from "../../assets/images/vignettes/3.webp";
+import vignette4 from "../../assets/images/vignettes/4.webp";
+import vignette5 from "../../assets/images/vignettes/5.webp";
+import vignette6 from "../../assets/images/vignettes/6.webp";
+import vignette7 from "../../assets/images/vignettes/7.webp";
+import vignette8 from "../../assets/images/vignettes/8.webp";
+import vignette9 from "../../assets/images/vignettes/9.webp";
+import vignette10 from "../../assets/images/vignettes/10.webp";
+import vignette11 from "../../assets/images/vignettes/11.webp";
+import vignette14 from "../../assets/images/vignettes/12.webp";
+import vignette15 from "../../assets/images/vignettes/13.webp";
+import vignette16 from "../../assets/images/vignettes/14.webp";
+import vignette17 from "../../assets/images/vignettes/15.webp";
+import vignette18 from "../../assets/images/vignettes/16.webp";
+import vignette19 from "../../assets/images/vignettes/17.webp";
 
 gsap.registerPlugin(ScrollTrigger);
-
-const getGrid = selector => {
-  let elements = gsap.utils.toArray(selector),
-    bounds,
-    getSubset = (axis, dimension, alternating, merge) => {
-      let a = [], 
-        subsets = {},
-        onlyEven = alternating === "even",
-        p;
-      bounds.forEach((b, i) => {
-        let position = Math.round(b[axis] + b[dimension] / 2),
-          subset = subsets[position];
-        subset || (subsets[position] = subset = []);
-        subset.push(elements[i]);
-      });
-      for (p in subsets) {
-        a.push(subsets[p]);
-      }
-      if (onlyEven || alternating === "odd") {
-        a = a.filter((el, i) => !(i % 2) === onlyEven);
-      }
-      if (merge) {
-        let a2 = [];
-        a.forEach(subset => a2.push(...subset));
-        return a2;
-      }
-      return a;
-    };
-  elements.refresh = () => bounds = elements.map(el => el.getBoundingClientRect());
-  elements.columns = (alternating, merge) => getSubset("left", "width", alternating, merge);
-  elements.rows = (alternating, merge) => getSubset("top", "height", alternating, merge);
-  elements.refresh();
-
-  return elements;
-};
 
 const PhotoGrid = () => {
   const gridRef = useRef(null);
   const lenis = useLenis();
 
-  const applyAnimation = (grid, animationType) => {
+  const photos = [
+    vignette1,
+    vignette2,
+    vignette3,
+    vignette4,
+    vignette5,
+    vignette6,
+    vignette7,
+    vignette8,
+    vignette9,
+    vignette10,
+    vignette11,
+    vignette14,
+    vignette15,
+    vignette16,
+    vignette17,
+    vignette18,
+    vignette19
+  ];
+  
+  const totalItems = photos.length;
+  
+  // Pré-calculer les valeurs aléatoires pour chaque item
+  const zValues = Array.from({ length: totalItems }, () => gsap.utils.random(-1600,200));
+  const xStartValues = Array.from({ length: totalItems }, () => gsap.utils.random(-1000,-500));
+  const xEndValues = Array.from({ length: totalItems }, () => gsap.utils.random(500,1000));
+
+  const applyAnimation = (grid) => {
     const gridWrap = grid.querySelector('.grid-wrap');
     const gridItems = grid.querySelectorAll('.grid__item');
     const gridItemsInner = [...gridItems].map(item => item.querySelector('.grid__item-inner'));
@@ -86,8 +76,7 @@ const PhotoGrid = () => {
         trigger: gridWrap,
         start: 'center center',
         end: 'bottom top',
-        scrub: true,
-        // markers: true, // Uncomment to see the trigger points
+        scrub: true
       }
     });
 
@@ -99,84 +88,83 @@ const PhotoGrid = () => {
         rotationY: 25
       })
       .set(gridItems, {
-        z: () => gsap.utils.random(-1600,200)
+        z: (i) => zValues[i]
       })
       .fromTo(gridItems, {
-        xPercent: () => gsap.utils.random(-1000,-500)
+        xPercent: (i) => xStartValues[i]
       }, {
-        xPercent: () => gsap.utils.random(500,1000)
+        xPercent: (i) => xEndValues[i]
       }, 0)
       .fromTo(gridItemsInner, {
         scale: 2
       }, {
-        scale: .5
+        scale: 0.5
       }, 0);
-      disappearTimeline
-      .to(gridItems, {
-        opacity: 0,
-        scale: 0,
-        yPercent: -100,
-        stagger: {
-          amount: 0.5,
-          from: "random"
-        }
-      });
+
+    disappearTimeline.to(gridItems, {
+      opacity: 0,
+      scale: 0,
+      yPercent: -100,
+      stagger: {
+        amount: 0.5,
+        from: "random"
+      }
+    });
   };
 
+  // Configurer ScrollTrigger avec Lenis via scrollerProxy
+  useLayoutEffect(() => {
+    if (!lenis) return;
 
-  useEffect(() => {
-    if (lenis) {
-      lenis.on('scroll', ScrollTrigger.update);
-    }
-    imagesLoaded(gridRef.current, { background: true }, () => {
-      applyAnimation(gridRef.current, 'type1');
+    // Définir ScrollTrigger pour utiliser Lenis comme scroller
+    ScrollTrigger.scrollerProxy(document.body, {
+      scrollTop(value) {
+        return arguments.length ? lenis.scrollTo(value, { immediate: true }) : lenis.scroll;
+      },
+      getBoundingClientRect() {
+        return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+      }
     });
+
+    // Boucle requestAnimationFrame pour synchroniser Lenis & ScrollTrigger
+    const raf = (time) => {
+      lenis.raf(time);
+      ScrollTrigger.update();
+      requestAnimationFrame(raf);
+    };
+    requestAnimationFrame(raf);
 
     return () => {
       ScrollTrigger.getAll().forEach(trigger => trigger.kill());
     };
   }, [lenis]);
 
-  const photos = [
-    vignette1,
-    vignette2,
-    vignette3,
-    vignette4,
-    vignette5,
-    vignette6,
-    vignette7,
-    vignette8,
-    vignette9,
-    vignette10,
-    vignette11,
-    vignette12,
-    vignette13,
-    vignette14,
-    vignette15,
-    vignette16,
-    vignette17,
-    vignette18,
-    vignette19
-  ];
+  // Charger les images avant d'appliquer l'animation
+  useEffect(() => {
+    if (!gridRef.current) return;
+
+    imagesLoaded(gridRef.current, { background: true }, () => {
+      applyAnimation(gridRef.current);
+      // Actualiser ScrollTrigger après l'initialisation de l'animation
+      ScrollTrigger.refresh();
+    });
+  }, [gridRef]);
 
   return (
-
-      <div className="content">
-        <div className="grid" ref={gridRef}>
-          <div className="grid-wrap">
-            {Array.from({ length: 19 }).map((_, i) => (
-              <div key={i} className="grid__item">
-                <div 
-                  className="grid__item-inner" 
-                  style={{
-                    backgroundImage: `url(${photos[i]})`
-                  }}
-                />
-              </div>
-            ))}
-          </div>
+    <div className="content">
+      <div className="grid" ref={gridRef}>
+        <div className="grid-wrap">
+          {Array.from({ length: totalItems }).map((_, i) => (
+            <div key={i} className="grid__item">
+              <div
+                className="grid__item-inner"
+                style={{ backgroundImage: `url(${photos[i]})` }}
+              />
+            </div>
+          ))}
         </div>
       </div>
+    </div>
   );
-}
+};
 export default PhotoGrid;
